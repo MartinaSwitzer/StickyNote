877d4ae5968754b8fff1d61e51d4b90c9a74983a
Martina Switzer <martinaswits@gmail.com>
2020-10-29 13:56:34 +0530

    StickyNote
    simple stickynote web application using html,css,javascript and reactjs.
    
    you can see the output of this project using the below link:
    
    https://stickynoteapp.netlify.app

---
+++ build/precache-manifest.68ce01d0afe530ed0fa5b1d90aa73ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588e86279b07fef36e1cb5347c15f390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6d2e9569a5e4777109",
+    "url": "/static/css/main.40e7bdda.chunk.css"
+  },
+  {
+    "revision": "012f290f1ef42684432d",
+    "url": "/static/js/2.41c4fbe2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.41c4fbe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6d2e9569a5e4777109",
+    "url": "/static/js/main.8c84c930.chunk.js"
+  },
+  {
+    "revision": "13d95b0b2356636d6e22",
+    "url": "/static/js/runtime-main.8eddd101.js"
+  }
+]);
